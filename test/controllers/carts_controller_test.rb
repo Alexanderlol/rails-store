f246fcedcd1c66e0a13f3dab065ba9337663041f
Alex Afeltra <afeltra.alex@gmail.com>
2017-01-09 00:35:28 +0000

    added blind fade to current line item with jquery-ui

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -39,6 +39,8 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy cart" do
+    post line_item_url, params: { product_id: products(:ruby).id }
+    @cart = Cart.find(session[:cart_id])
     assert_difference('Cart.count', -1) do
       delete cart_url(@cart)
     end
