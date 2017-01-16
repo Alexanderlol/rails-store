66f7a350e39d2d889f43b075f2d8037976904256
Alex Afeltra <afeltra.alex@gmail.com>
2017-01-16 21:52:19 +0000

    used action cable to update catalog display when product changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,9 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  
+  #disable check for multiple streams
+  config.action_cable.disable_request_forgery_protection = true
 
   # Do not eager load code on boot.
   config.eager_load = false
