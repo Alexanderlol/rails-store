2b2bb0cd5f9e6e47e3f46b7588bcf4811e7e9920
Alex Afeltra <afeltra.alex@gmail.com>
2017-01-20 07:03:19 +0000

    config'd environments to enable sending confirmation emails. tailored mailer to send conf emails in plain text and html

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,20 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+  
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    
+    config.action_mailer.smtp_settings = {
+      address:             "smtp.gmail.com",
+      port:                587,
+      domain:              "domain.of.sender.net",
+      authentication:      "plain",
+      user_name:           "dave",
+      password:            "secret",
+      enable_starttls_auto: true
+    }
+  end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
